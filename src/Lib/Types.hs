c756d5eed9e8ce6a1b981443adf2d0e8f3587668
Francososa <franky.sosa@gmail.com>
2018-09-21 17:37:54 -0600

    Define Aeson types

---
+++ src/Lib/Types.hs
@@ -0,0 +1,50 @@
+module Lib.Types where
+
+import Lib
+import Data.Aeson
+import Data.Text as T
+import Data.ByteString.Lazy as B
+import Data.ByteString.Lazy.Char8 as BC
+import GHC.Generics
+
+
+data NOAAResult = NOAAResult
+  { uid :: T.Text
+  , mindate :: T.Text
+  , maxdate :: T.Text
+  , name :: T.Text
+  , datacoverage :: Int
+  , resultId :: T.Text
+  } deriving Show
+
+instance FromJSON NOAAResult where
+  parseJSON (Object v) =
+    NOAAResult <$> v .: "uid"
+               <*> v .: "mindate"
+               <*> v .: "maxdate"
+               <*> v .: "name"
+               <*> v .: "datacoverage"
+               <*> v .: "id"
+
+
+
+data Resultset = Resultset
+  { offset :: Int
+  , count :: Int
+  , limit :: Int } deriving (Show,Generic)
+
+instance FromJSON Resultset
+
+
+data Metadata = Metadata
+  { resultset :: Resultset } deriving (Show,Generic)
+
+instance FromJSON Metadata
+
+
+data NOAAResponse = NOAAResponse
+  { metadata :: Metadata
+  , results :: [NOAAResult]
+  } deriving (Show,Generic)
+
+instance FromJSON NOAAResponse
